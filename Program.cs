691a38d208a0c09ef238e85b4a70d27f8a0e40fa
ZiyodullaSodiqov <ziyodullasodiqov2007@gmail.com>
2023-05-30 21:02:30 +0500

    MyFirstApp in C# Dotnet

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//     int firstNumber = Convert.ToInt32(Console.ReadLine());
+//     int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//     System.Console.WriteLine($"Add {firstNumber + secondNumber}");
+
+// System.Console.Write("Ismingiz: ");
+// string name = System.Console.ReadLine();
+// System.Console.WriteLine($"Hello {name}");
+
+// int kescha = 2;
+
+// System.Console.Write("Yoshingiz: ");
+// int yoshim = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine($"Yoshingiz farqi: {yoshim - kescha}");
+
+System.Console.Write("Raqam A: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Raqam B: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if(numberOne == numberTwo) {
+    System.Console.WriteLine("Sonlar teng");
+} else {
+    System.Console.WriteLine("Sonlari teng emas");
+}
